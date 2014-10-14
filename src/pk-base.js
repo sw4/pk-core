a1dddb99746b541d2906a3a850197b577475c66b
sw4 <samuel.wheeler@gmail.com>
2014-10-14 14:39:04 +0100

    Update pk-base.js

--- src/pk-base.js
+++ src/pk-base.js
@@ -70,7 +70,7 @@ var pk = pk || {};
         while (el = el[prop]) { ++i; }
         return i-1;        
     }; 
-    pk.replaceEl(el,str) {
+    pk.replaceEl = function(el,str) {
         var newEl = pk.createEl(str); 
         foreach (el.attributes as a) {
             newEl.setAttribute(a.nodeName, a.nodeValue);
